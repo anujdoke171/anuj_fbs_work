dcbe40be6743104014c4f1194599b068ea9ffa6a
anujdoke171 <dokeanuj171@gmail.com>
2025-07-15 10:42:20 +0530

    Add files via upload

---
+++ C Programming/Test on c/test2/q1.c
@@ -0,0 +1,19 @@
+void main()
+{
+	int units;
+	printf("enter the units");
+	scanf("%d",&units);
+	if(units<=50)
+	{
+		printf("the 30rs.per unit");
+	}
+	else if(units<=150)
+	{
+		printf("the 40rs.per unit");
+	}
+	else 
+	{
+		printf("the 50rs. per unit");
+	}
+	
+}
